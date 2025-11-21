f2139224e94470346083aba221a3c537e16df626
xLorenz <rivelorenzo41@gmail.com>
2025-11-21 14:35:10 -0300

    foundation

---
+++ GamePanel.java
@@ -0,0 +1,11 @@
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
